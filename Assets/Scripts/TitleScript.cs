86717292c6e4321fe1b179a3f464eab0b01769fa
radunsb <radunsb@gmail.com>
2024-01-30 21:02:58 -0500

    comments

--- Assets/Scripts/TitleScript.cs
+++ Assets/Scripts/TitleScript.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 
 public class TitleScript : MonoBehaviour
 {
+    //List of the possible menu options
     public GameObject[] buttons;
-    int _buttonActive = -1;
+    //Menu option that is currently being "hovered" over
+    int _buttonActive = -1; 
+    //Color for a non-hovered button
     Color inactiveButton;
+    //Color for a hovered button
     Color activeButton;
     void Start()
     {
@@ -19,20 +23,25 @@ public class TitleScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            //Scroll one button up
             updateActiveButton(-1);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            //Scroll one button down
             updateActiveButton(1);
         }
     }
 
     void updateActiveButton(int inputNum)
     {
+        //If the user doesn't have an option selected, pushing any navigational key will
+        //just select the first button no matter what
         if(_buttonActive == -1)
         {
             _buttonActive = 0;
         }
+        //Otherwise, scroll. Variable used so this will still work if we add more buttons
         else
         {
             int numButtons = buttons.Length;
@@ -43,6 +52,8 @@ public class TitleScript : MonoBehaviour
 
     void updateButtonColors()
     {
+        //Update each button color only when the selected one is changed (runs from
+        //updateActiveButton())
         for(int i = 0; i < buttons.Length; i++)
         {
             if(i != _buttonActive)

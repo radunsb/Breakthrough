2725383d97ce251c33c2e51067c78ed5f002f387
radunsb <radunsb@gmail.com>
2024-02-03 11:32:26 -0500

    Significant Attack Work
    The controls are not perfect, but you are able to actually have attacks hit opponents. Will probably change a few things when implementing it in game, but works well for testing purposes so far!

---
+++ Assets/Scripts/HitboxScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class HitboxScript : MonoBehaviour
+{
+    public float velocityMult;
+    public float launchDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

667d91b7f24ea640e6ce9188991488e6f41c1636
radunsb <radunsb@gmail.com>
2024-02-07 21:41:06 -0500

    UAir and FAir

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -73,7 +73,7 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool("Grounded", _grounded);
     }
     private void FixedUpdate()
     {

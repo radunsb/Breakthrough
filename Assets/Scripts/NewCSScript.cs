16b70c00fbc37b28f3b47c26a0faaf5a09918c70
radunsb <radunsb@gmail.com>
2024-03-21 23:11:31 -0400

    BGM and other stage setup
    for the subway stage i made all of the prefabs for the different screens, they just need the breakable wall sprites to overlay. tower still needs everything.

--- Assets/Scripts/NewCSScript.cs
+++ Assets/Scripts/NewCSScript.cs
@@ -79,7 +79,18 @@ public class NewCSScript : MonoBehaviour
             PlayerPrefs.SetInt("Stage", matchInfo[2]);
             PlayerPrefs.SetInt("P1 Points", 0);
             PlayerPrefs.SetInt("P2 Points", 0);
-            SceneManager.LoadScene("MatchScene");
+            PlayerPrefs.SetString("Gameplay Scene", "TowerScene");
+            SceneManager.LoadScene("TowerScene");
+        }
+        else if (matchInfo[0] != -1 && matchInfo[1] != -1 && matchInfo[2] == 5)
+        {
+            PlayerPrefs.SetInt("Player1Char", matchInfo[0]);
+            PlayerPrefs.SetInt("Player2Char", matchInfo[1]);
+            PlayerPrefs.SetInt("Stage", matchInfo[2]);
+            PlayerPrefs.SetInt("P1 Points", 0);
+            PlayerPrefs.SetInt("P2 Points", 0);
+            PlayerPrefs.SetString("Gameplay Scene", "SubwayScene");
+            SceneManager.LoadScene("SubwayScene");
         }
         if (matchInfo[0] != -1 && matchInfo[1] != -1 && matchInfo[2] == 4)
         {
